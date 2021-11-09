93deeafafc9dea43eed5bc6b9c0a56a58b3a59a2
SirTLD <ekanemita@gmail.com>
2021-11-08 20:33:18 -0600

    update to sorting and filtering methods

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { productItems } from './Data';
 import ProductItem from './ProductItem';
 import { mobile } from '../responsive';
+import axios from 'axios';
 
 const Container = styled.div`
   display: flex;
@@ -13,13 +14,63 @@ const Container = styled.div`
   ${mobile({ padding: '0', width: '100vw' })}
 `;
 
-const Products = () => {
+const Products = ({ category, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get(
+          category
+            ? `http://localhost:5000/api/products?category=${category}`
+            : 'http://localhost:5000/api/products'
+        );
+        setProducts(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, [category]);
+
+  useEffect(() => {
+    category &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, category, filters]);
+
+  useEffect(() => {
+    if (sort === 'New Arrivals') {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === 'asc') {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <>
       <Container>
-        {productItems.map((item) => (
-          <ProductItem item={item} key={item.id} />
-        ))}
+        {category
+          ? filteredProducts.map((item) => (
+              <ProductItem item={item} key={item.id} />
+            ))
+          : products
+              .slice(0, 8)
+              .map((item) => <ProductItem item={item} key={item.id} />)}
       </Container>
     </>
   );
