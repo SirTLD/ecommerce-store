42b95a02b30f2dfdf989dfaaa60affbceaf74f2b
SirTLD <ekanemita@gmail.com>
2021-11-11 23:38:35 -0600

    redux persist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
-import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
